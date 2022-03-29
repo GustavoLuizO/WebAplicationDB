d190c12e08deec4ad1317967c53368bdde3b73d2
Gustavo Luiz <glogustavo4321@gmail.com>
2022-03-29 19:02:54 -0300

    Create Migration
    Criação da migrations para depois upar no bd

---
+++ WebAplicationProduct/WebAplicationProduct/Migrations/20220329125735_Inicial-comeco.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAplicationProduct.Migrations
+{
+    public partial class Inicialcomeco : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Fabricantes",
+                columns: table => new
+                {
+                    FabricanteID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fabricantes", x => x.FabricanteID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    ProdutoID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Preco = table.Column<float>(type: "real", nullable: true),
+                    FabricanteID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.ProdutoID);
+                    table.ForeignKey(
+                        name: "FK_Produtos_Fabricantes_FabricanteID",
+                        column: x => x.FabricanteID,
+                        principalTable: "Fabricantes",
+                        principalColumn: "FabricanteID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produtos_FabricanteID",
+                table: "Produtos",
+                column: "FabricanteID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+
+            migrationBuilder.DropTable(
+                name: "Fabricantes");
+        }
+    }
+}
